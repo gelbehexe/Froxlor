b0f11f070caf0b3655b5713a97f8da3a51990fe8
Michael Kaufmann <d00p@froxlor.org>
2022-01-07 15:38:12 +0100

    Cherry-picks from version to 0.10.32

--- install/updates/froxlor/0.10/update_0.10.inc.php
+++ install/updates/froxlor/0.10/update_0.10.inc.php
@@ -965,3 +965,14 @@ if (\Froxlor\Froxlor::isFroxlorVersion('0.10.30-local-0')) {
     showUpdateStep("Updating from 0.10.30-local-0 to 0.10.31-local-0", false);
     \Froxlor\Froxlor::updateToVersion('0.10.31-local-0');
 }
+
+if (\Froxlor\Froxlor::isFroxlorVersion('0.10.31-local-0')) {
+        showUpdateStep("Updating from 0.10.31-local-0 to 0.10.32-local-0", false);
+        \Froxlor\Froxlor::updateToVersion('0.10.32-local-0');
+}
+
+if (\Froxlor\Froxlor::isDatabaseVersion('202111040-local-0')) {
+    Settings::AddNew("system.phpindexlast", '0');
+    lastStepStatus(0);
+    \Froxlor\Froxlor::updateToDbVersion('202112310-local-0');
+}
--- lib/Froxlor/Froxlor.php
+++ lib/Froxlor/Froxlor.php
@@ -7,7 +7,7 @@ final class Froxlor
 {
 
 	// Main version variable
-	const VERSION = '0.10.31-local-0';
+	const VERSION = '0.10.32-local-0';
 
 	// Database version (YYYYMMDDC where C is a daily counter)
 	const DBVERSION = '202109040-local-0';
